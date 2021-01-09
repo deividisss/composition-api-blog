187548cf4d567c28c53a888319ec3c73b796b403
Deividas <deivis124@gmail.com>
2021-01-09 09:17:42 +0200

    testing ref, reactive, computed, watch, watchEffect in Composition API

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,100 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Home</h1>
+    <h1>Refs</h1>
+    <p>My name is {{ ninjaOne.name }} - {{ ninjaOne.age }}</p>
+    <button @click="updateNinjaOne">Click me</button>
+    <h2>Reactivee</h2>
+    <p>{{ ninjaTwo.name }} - {{ ninjaTwo.age }}</p>
+    <button @click="updateNinjaTwo">Click me</button>
+    <br />
+    <br />
+    <button @click="test">Test</button>
+    <hr />
+    <h2>Computed</h2>
+
+    <input type="text" v-model="search" />
+    <p>search term - {{ search }}</p>
+    <div v-for="name in matchingNames" :key="name">
+      <p>{{ name }}</p>
+    </div>
+
+    <button @click="handleClick">Stop watching</button>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import { ref, reactive, computed, watch, watchEffect } from "vue";
 
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+  name: "Home",
+  setup() {
+    // Setup allways runs first
+    // Reactive don't work on primitive type
+    const ninjaOne = ref({ name: "mario", age: 30 });
+    const ninjaTwo = reactive({ name: "luigi", age: 35 });
+
+    const updateNinjaOne = () => {
+      ninjaOne.value.age = 40;
+    };
+
+    const updateNinjaTwo = () => {
+      ninjaTwo.age = 45;
+    };
+
+    // Computed properties
+    const search = ref("");
+    const names = ref([
+      "mario",
+      "yoshi",
+      "luigi",
+      "toad",
+      "bowser",
+      "koopa",
+      "peach",
+    ]);
+
+    const stopWatch = watch(search, () => {
+      console.log("watch function ran");
+    });
+
+    // Allways runs initially
+    const stopEffect = watchEffect(() => {
+      console.log("watchEffect function ran", search.value);
+    });
+
+    const matchingNames = computed(() => {
+      return names.value.filter((name) => name.includes(search.value));
+    });
+
+    const handleClick = () => {
+      stopWatch();
+      stopEffect();
+    };
+
+    return {
+      ninjaOne,
+      updateNinjaOne,
+      ninjaTwo,
+      updateNinjaTwo,
+      names,
+      search,
+      matchingNames,
+      handleClick,
+    };
+  },
+  data() {
+    return {
+      // Reactive values
+    };
+  },
+  methods: {
+    test() {
+      // console.log(this.age);
+      this.ninjaTwo.age = 9;
+      this.ninjaOne.age = 9;
+      // console.log(this.age);
+    },
+  },
+};
 </script>
