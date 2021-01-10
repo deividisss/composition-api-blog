9e987bc097e24416cf558d53381cb46411a656a2
Deividas <deivis124@gmail.com>
2021-01-10 07:56:38 +0200

    Using props in setup

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,100 +1,31 @@
 <template>
   <div class="home">
     <h1>Home</h1>
-    <h1>Refs</h1>
-    <p>My name is {{ ninjaOne.name }} - {{ ninjaOne.age }}</p>
-    <button @click="updateNinjaOne">Click me</button>
-    <h2>Reactivee</h2>
-    <p>{{ ninjaTwo.name }} - {{ ninjaTwo.age }}</p>
-    <button @click="updateNinjaTwo">Click me</button>
-    <br />
-    <br />
-    <button @click="test">Test</button>
-    <hr />
-    <h2>Computed</h2>
-
-    <input type="text" v-model="search" />
-    <p>search term - {{ search }}</p>
-    <div v-for="name in matchingNames" :key="name">
-      <p>{{ name }}</p>
-    </div>
-
-    <button @click="handleClick">Stop watching</button>
+    <PostList :posts="posts" />
   </div>
 </template>
 
 <script>
-import { ref, reactive, computed, watch, watchEffect } from "vue";
+import PostList from "../components/PostList";
+import { ref } from "vue";
 
 export default {
   name: "Home",
+  components: { PostList },
   setup() {
-    // Setup allways runs first
-    // Reactive don't work on primitive type
-    const ninjaOne = ref({ name: "mario", age: 30 });
-    const ninjaTwo = reactive({ name: "luigi", age: 35 });
-
-    const updateNinjaOne = () => {
-      ninjaOne.value.age = 40;
-    };
-
-    const updateNinjaTwo = () => {
-      ninjaTwo.age = 45;
-    };
-
-    // Computed properties
-    const search = ref("");
-    const names = ref([
-      "mario",
-      "yoshi",
-      "luigi",
-      "toad",
-      "bowser",
-      "koopa",
-      "peach",
+    const posts = ref([
+      {
+        title: "welcome to the blog",
+        body:
+          "  Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus cum, esse repudiandae odio ducimus sed aliquid quo hic at corrupti eveniet id delectus architecto dolorum earum repellat atque expedita odit rem. Dolor iure necessitatibus reprehenderit aut pariatur distinctio, dolorum, fugit tempore optio minus totam atque qui magni commodi a animi dignissimos deserunt. Quidem iure animi veniam reiciendis debitis in adipisci, delectus at ipsum, quam quisquam incidunt nemo. Dolorem obcaecati iste ullam dolores neque consequuntur, voluptatem libero tempora atque molestias qui, exercitationem iure? Blanditiis error excepturi quo culpa praesentium numquam animi sed consequatur maiores. Culpa quod maiores corporis, quo totam fugiat possimus ea saepe consequuntur cum soluta reiciendis illum dolores maxime odio debitis, excepturi incidunt tempore. Praesentium architecto eaque voluptatum fugit incidunt quaerat asperiores harum cumque saepe, tenetur nam numquam sed doloremque sequi ad exercitationem? Ducimus ea ad pariatur voluptates labore voluptatibus delectus dicta sed possimus consectetur, similique sit minima accusantium repellat distinctio, officiis voluptatem animi quos praesentium magnam. Dolores, sit quidem eaque omnis nulla ullam enim modi possimus voluptatibus deserunt odit. Commodi architecto adipisci ut veniam earum repellendus corporis similique iste, temporibus, neque eaque saepe distinctio. Repellat corporis mollitia commodi quisquam rem quae voluptatum dolorum, consequuntur, molestiae, eius ipsam ipsum aperiam est architecto maxime nihil dolore! Sequi voluptates iusto architecto mollitia consequatur dicta veniam, quae quisquam, nisi quia quis porro laboriosam quos. Ad perferendis veritatis nobis molestiae maxime iusto temporibus assumenda quas, hic dolore quo quisquam magnam voluptate distinctio enim quos optio vero saepe similique? Rem ipsam quisquam cum officiis voluptatem labore minima, accusamus veniam ut temporibus soluta ipsa consectetur nostrum quibusdam deserunt nulla vitae excepturi nihil sint? Expedita possimus accusantium ex, facilis placeat quia aperiam harum quidem voluptatem, repudiandae id repellendus non deleniti modi, adipisci rerum magnam iusto quam ducimus ullam soluta assumenda tenetur? Quis iste vero ab quasi maiores distinctio. Ipsum atque quod commodi, ut dolor aut id exercitationem quia quaerat iure impedit laboriosam illo aliquid eius beatae autem cumque non officiis reprehenderit minima? Delectus beatae natus voluptates, minus doloribus commodi dolor dolores? Totam dolor suscipit laborum atque explicabo vitae commodi, animi dolores similique, maiores ut nisi. Minima voluptates et, quisquam vel doloribus natus veritatis vitae, omnis libero praesentium nam ducimus assumenda, dicta veniam perferendis ipsam. Iure suscipit accusamus voluptatem fugit iste ut deleniti fuga odit, aut tenetur distinctio deserunt placeat at cumque repellendus, delectus fugiat repudiandae quibusdam error enim itaque a architecto. Excepturi accusantium dolores a reiciendis molestiae! Placeat praesentium ab, facere cupiditate magni maiores rerum? Eius voluptatum ut eveniet tempora rem assumenda libero nesciunt possimus laborum optio pariatur accusantium, ullam qui temporibus quidem maiores amet odit sit dolores repudiandae officia perferendis voluptas iste. Nesciunt nam officia ducimus mollitia inventore voluptatibus, similique, nemo esse nisi impedit quisquam illum expedita voluptatum! Repellat unde harum, soluta explicabo quidem eum quod amet numquam deleniti culpa vero et non voluptatum cupiditate ea omnis odit sapiente recusandae earum qui quis voluptas laborum ullam nemo. Id ex quidem voluptate dolores, nisi aliquid laboriosam exercitationem debitis incidunt. Asperiores, sit unde quas esse maxime repellendus",
+        id: 1,
+      },
+      { title: "top 5 CSS tips", body: "Lorem ipsum", id: 2 },
     ]);
 
-    const stopWatch = watch(search, () => {
-      console.log("watch function ran");
-    });
-
-    // Allways runs initially
-    const stopEffect = watchEffect(() => {
-      console.log("watchEffect function ran", search.value);
-    });
-
-    const matchingNames = computed(() => {
-      return names.value.filter((name) => name.includes(search.value));
-    });
-
-    const handleClick = () => {
-      stopWatch();
-      stopEffect();
-    };
-
-    return {
-      ninjaOne,
-      updateNinjaOne,
-      ninjaTwo,
-      updateNinjaTwo,
-      names,
-      search,
-      matchingNames,
-      handleClick,
-    };
-  },
-  data() {
-    return {
-      // Reactive values
-    };
-  },
-  methods: {
-    test() {
-      // console.log(this.age);
-      this.ninjaTwo.age = 9;
-      this.ninjaOne.age = 9;
-      // console.log(this.age);
-    },
+    return { posts };
   },
 };
 </script>
+
+<style></style>
